3e3b109e3265ba5d0bd834f7990a937deab0bba2
徳留 美帆 <tokudome@pepabo.com>
2024-01-14 20:06:32 +0900

    気象データを取得できるようにした

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-document.getElementById("test").innerHTML="JavaScript テスト"
+const weatherForm = document.getElementById("weather-form")
+const cityInput = document.getElementById("city-input")
+const weatherResults = document.getElementById("weather-results")
+fetch("https://api.weatherapi.com/v1/current.json?key=c9b135c00c9c4c8cbee143639241301&q=kyoto&aqi=no")
+  .then(response =>response.json())
+  .then(jsonData =>console.log(jsonData))
